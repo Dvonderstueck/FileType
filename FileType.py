d4a347a9eacf3ef49bacdbdbffe50fb65a37adac
Dvonderstueck <dennis.vonderstueck@grandcentrix.net>
2023-10-13 08:39:34 +0200

    version 2.0

--- FileType.py
+++ FileType.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import sys
+from typing import List
 
-def read_file_header(path):
+def read_file_header(path: str) -> bytes:
     with open(path, 'rb') as file:
         header = file.read()
     return header
 
-def find_file_type(header):
+def find_file_type(header: bytes) -> str:
     magic_numbers = {
         b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A': 'PNG',
         b'\xFF\xD8\xFF': 'JPEG',
@@ -20,7 +21,7 @@ def find_file_type(header):
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script_name.py <file_path>")
-        sys.exit()  
+        sys.exit()
 
     file_path = sys.argv[1]
 
